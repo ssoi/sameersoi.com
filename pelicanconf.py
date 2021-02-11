ff78e8278b772ec28f5815e44ee62740541dc306
ssoi <sameersoi@gmail.com>
2021-02-10 19:21:32 -0800

    style(pelican): run black on pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,23 +4,23 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = 'Sameer Soi'
-SITENAME = 'Sameer Soi'
-SITEURL = ''
+AUTHOR = "Sameer Soi"
+SITENAME = "Sameer Soi"
+SITEURL = ""
 
-THEME = os.environ['THEMEDIR']
+THEME = os.environ["THEMEDIR"]
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'America/Los_Angeles'
+TIMEZONE = "America/Los_Angeles"
 
-DEFAULT_LANG = 'en'
-DEFAULT_DATE = 'fs'
+DEFAULT_LANG = "en"
+DEFAULT_DATE = "fs"
 
-MARKUP = ('md', 'ipynb')
+MARKUP = ("md", "ipynb")
 PELICANCONF_PATH = os.path.dirname(os.path.realpath(__file__))
-PLUGIN_PATHS = [os.path.join(PELICANCONF_PATH, 'plugins')]
-PLUGINS = ['ipynb.markup']
+PLUGIN_PATHS = [os.path.join(PELICANCONF_PATH, "plugins")]
+PLUGINS = ["ipynb.markup"]
 
 IGNORE_FILES = [".ipynb_checkpoints"]
 
@@ -35,12 +35,14 @@ AUTHOR_FEED_RSS = None
 LINKS = ()
 
 # Social widget
-SOCIAL = (('github', 'ssoi'),
-          ('stackexchange', '328657/sameer'),
-          ('twitter', 'sameersoi'),
-          ('linkedin', 'sameer-soi-91a895b1'),)
+SOCIAL = (
+    ("github", "ssoi"),
+    ("stackexchange", "328657/sameer"),
+    ("twitter", "sameersoi"),
+    ("linkedin", "sameer-soi-91a895b1"),
+)
 
 DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
