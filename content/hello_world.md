d3cae2b610955c313665b819cfcc13325932837d
ssoi <sameersoi@gmail.com>
2021-06-16 22:55:56 -0700

    feat(theme): move to new theme

--- content/hello_world.md
+++ content/hello_world.md
@@ -1,6 +1,5 @@
-Title: Hello, World!
-Date: 2018-03-25 19:31
-Category: Intro
+title: Hello, World!
+date: 2018-03-25 19:31
 
 Hello, world! This is my innagural post to my personal blog. More than an exercise in blogging, I am using this to play around with simple web and DevOps technologies. This static site is served via nginx and hosted on a VPS, which is [provisioned](https://github.com/ssoi/personal-deploy) using [Ansible](https://www.ansible.com/). The static site is generated using [Pelican](https://blog.getpelican.com/). Finally, the site itself is tested and deployed to the VPS using [Travis CI](https://travis-ci.org).
 
--- pelicanconf.py
+++ pelicanconf.py
@@ -14,9 +14,11 @@ PATH = "content"
 
 TIMEZONE = "America/Los_Angeles"
 
+AVATAR = "https://secure.gravatar.com/avatar/28510cc609a757ebf02fe6db2058cfb7?size=500"
+
 DEFAULT_LANG = "en"
 DEFAULT_DATE = "fs"
-
+DISPLAY_CATEGORIES_ON_MENU = False
 MARKUP = ("md", "ipynb")
 PELICANCONF_PATH = os.path.dirname(os.path.realpath(__file__))
 PLUGIN_PATHS = [os.path.join(PELICANCONF_PATH, "plugins")]
@@ -35,14 +37,9 @@ AUTHOR_FEED_RSS = None
 LINKS = ()
 
 # Social widget
-SOCIAL = (
-    ("github", "ssoi"),
-    ("stackexchange", "328657/sameer"),
-    ("twitter", "sameersoi"),
-    ("linkedin", "sameer-soi-91a895b1"),
-)
+SOCIAL = (("github", "ssoi"), ("twitter", "sameersoi"), ("linkedin", "sameer-soi"))
 
 DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
